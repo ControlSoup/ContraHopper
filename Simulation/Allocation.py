428ac1e0a95bf5f8b719e1451e6f4407ec482cdc
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-09-04 12:00:18 -0700

    Updated File Structure
    File Structures for future

---
+++ Simulation/Allocation.py
@@ -0,0 +1,13 @@
+# file for allocation
+
+def allocator(ControlOutput):
+
+    #Need fake thrust curve
+    #Fake Voltage curves
+    #Fake Torque from vanes
+
+
+    return None
+
+def allocator_sim(ControlOutput):
+return None
