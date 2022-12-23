0e7094ae948d86bc994872807156a4985b756ae7
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-12-23 08:08:55 +0100

    playing around with C

--- Simulation/6dof_head.h
+++ Simulation/6dof_head.h
@@ -1,14 +1,10 @@
 #include<stdio.h>
 #include<math.h>
-#include"../../linalg/vector.h"
-#include"../../linalg/matrix.h"
-#include"../../linalg/linsolve.h"
-#include"../../linalg/eigen.h"
-#include"../../linalg/linreg.h"
-#include"../../linalg/rand.h"
+#include"../../CTools/pythonish_functions.h"
 
 
 /*
 Non standard Dependencies:
-    madury/linalg : https://github.com/madrury/linalg
+    pythonish_functions.h
 */
+
