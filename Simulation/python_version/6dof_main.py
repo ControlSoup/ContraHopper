9b8504ba61c0b0d01487d1c05015d65dfe74bc50
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-12-27 19:02:47 +0100

    Cleaned up sim, added some basic plotting, but plotting needs improvment

--- Simulation/python_version/6dof_main.py
+++ Simulation/python_version/6dof_main.py
@@ -1,92 +1,119 @@
 import numpy as np
 import Kinematics
+import strapdown
+from matplotlib import pyplot as plt
+"""
+===========================
+Inputs
+===========================plot_position
+"""
+# Sim Options
+sim_frequency    = 500.0
+start_time       = 0.0
+end_time         = 10.0
 
-"""
-===========================
-Magic Numbers
-===========================
-"""
-in2m = 0.0254
-kg2n = 9.8055
+# State Variables 
+init_position_m   = np.zeros(3)
+init_velocity_mps = np.array([1,0,0.])
+init_Cb2i_dcm     = np.identity(3)
+init_w_radps      = np.array([0,0,0])
 
-"""
-===========================
-Sim Options
-===========================
-"""
-sim_frequency = 500
-start_time = 0
-end_time = 15
-isPlotting = False
-is3d = False
-
-dt = 1 / sim_frequency
-sim_t = np.arange(start_time, end_time, dt)
+# Inputs
+init_forces_n     = np.zeros(3)
+init_moments_nm   = np.zeros(3)
 
 """
 ===========================
 Initialization
 ===========================
-"""
+""" 
+CurrentState = Kinematics.State(position_m   = init_position_m, 
+                                velocity_mps = init_velocity_mps,
+                                Cb2i_dcm     = init_Cb2i_dcm,
+                                w_radps      = init_w_radps)
 
+CurrentInputs = Kinematics.Inputs(forces_n = init_forces_n,
+                                  moments_nm = init_moments_nm)
 
-class State:
-    def __init__(self, position_m, velocity_mps, attitude_Cb2i_dcm, w_radps):
-        self.position_m = np.array(position_m)
-        self.velocity_mps = np.array(velocity_mps)
-        self.attitude_Cb2i_dcm = np.array(attitude_Cb2i_dcm)
-        self.w_radps = np.array(w_radps)
+ContraHopper = Kinematics.MassProperties(mass_kg=10, 
+                                         i_tensor_cg=[[1, 0, 0], 
+                                                      [0, 1, 0], 
+                                                      [0, 0, 1.]])
 
-
-class Inputs:
-    def __init__(self, forces_n, moments_nm):
-        self.forces_n = np.array(forces_n)
-        self.moments_nm = np.array(moments_nm)
-
-
-class VehicleProperties:
-    def __init__(self, mass_kg, i_tensor_cg):
-        self.mass_kg = mass_kg
-        self.i_tensor_cg = np.array(i_tensor_cg)
-
-
-CurrentState = State(position_m=np.zeros(3), velocity_mps=np.zeros(3), attitude_Cb2i_dcm=np.identity(3),
-                     w_radps=np.zeros(3))
-
-CurrentInputs = Inputs(forces_n=np.zeros(3),
-                       moments_nm=np.zeros(3))
-
-ContraHopper = VehicleProperties(mass_kg=10, i_tensor_cg=[[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-Gyroscope = VehicleProperties(mass_kg=0.26, i_tensor_cg=[[0.7, 0, 0.7], [0, 1, 0], [0.7, 0, -0.7]])
-
-g_mps2 = 0
+Gyroscope = Kinematics.MassProperties(mass_kg=0.26, 
+                                      i_tensor_cg=[[0.7,0,0.7], 
+                                                   [0.0,1,0.0], 
+                                                   [0.7,0,-0.7]])
+dt = 1/sim_frequency
+itt_sim = np.arange(start_time,end_time,dt)
 
 """
 ===========================
 Data Collection
-===========================
+=========================== 
 """
-stash_array = np.zeros((len(sim_t), 14))
+# Pre-allocation
+stash_position_m   = np.zeros((len(itt_sim),len(CurrentState.position_m)))
+stash_velocity_mps = np.zeros((len(itt_sim),len(CurrentState.velocity_mps)))
+stash_Cb2o_dcm_a   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[0])))
+stash_Cb2o_dcm_b   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[0])))
+stash_Cb2o_dcm_c   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[0])))
 
 """
 ===========================
 Sim Loop
 ===========================
 """
-CurrentState.w_radps = np.array([0, 10, 0])
-for i in sim_t:
+for i in range(len(itt_sim)):
 
+    #Update State
+    CurrentState.update_from_state_vector(Kinematics.rk4(CurrentState.state_vector, CurrentInputs, ContraHopper, dt))
+    CurrentState.Cb2i_dcm = strapdown.orthonormalize(CurrentState.Cb2i_dcm) 
 
+    # Store state as a state_vector for plotting
+    stash_position_m[i] = CurrentState.position_m
+    stash_velocity_mps[i] = CurrentState.velocity_mps
+    stash_Cb2o_dcm_a   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[0])))
+    stash_Cb2o_dcm_b   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[1])))
+    stash_Cb2o_dcm_c   = np.zeros((len(itt_sim),len(CurrentState.Cb2i_dcm[2])))
+    
 
-
-    Kinematics.get_new_state(CurrentState, CurrentInputs, g_mps2, Gyroscope, dt)
 
 """
 ===========================
 Data Presentation
 ===========================
 """
-print(CurrentState.position_m)
-print(CurrentState.velocity_mps)
-print(CurrentState.attitude_Cb2i_dcm)
-print(CurrentState.w_radps)
+plot_2d          = False
+plot_position_3d = True
+print_final      = False
+
+if plot_2d:
+    fig, axs = plt.subplots(2)
+    axs[0].plot(itt_sim, stash_position_m[:,0],label = "Actual X",c = "red")
+    axs[0].plot(itt_sim, stash_position_m[:,1],label = "Actual Y",c = "green")
+    axs[0].plot(itt_sim, stash_position_m[:,2],label = "Actual Z",c = "blue")
+    axs[0].set_title("Position (m)")
+    axs[0].legend()
+    axs[1].plot(itt_sim, stash_velocity_mps[:,0],label = "Actual Xdot",c = "red")
+    axs[1].plot(itt_sim, stash_velocity_mps[:,1],label = "Actual Ydot",c = "green")
+    axs[1].plot(itt_sim, stash_velocity_mps[:,2],label = "Actual Zdot",c = "blue")
+    axs[1].set_title("Velocity (m)")
+    axs[1].legend()
+    plt.show()
+
+if plot_position_3d:
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot(stash_position_m[:,0],stash_position_m[:,1],stash_position_m[:,2], label='Position (m)')
+    ax.legend()
+    plt.show()
+
+if print_final:
+    print(f'Final Position (m)         = {CurrentState.position_m}\n')
+    print(f'Final Velocity (m/s)       = {CurrentState.velocity_mps}\n')
+    print(f'Final Attitude (dcm)       = {CurrentState.Cb2i_dcm[0]}')
+    print(f'                             {CurrentState.Cb2i_dcm[1]}')
+    print(f'                             {CurrentState.Cb2i_dcm[2]}\n')
+    print(f'Final Attitude (deg)       = {strapdown.dcm2euler(CurrentState.Cb2i_dcm)}\n')
+    print(f'Final Angular Rate (rad/s) = {CurrentState.w_radps}')
+
--- Simulation/python_version/Kinematics.py
+++ Simulation/python_version/Kinematics.py
@@ -9,75 +9,97 @@ Support Functions
 ===========================
 """
 
+class State:
+    '''
+    Stores state in variable and vector format
+    '''
+    def __init__(self,position_m=np.zeros(3),velocity_mps=np.zeros(3),
+                 Cb2i_dcm=np.identity(3),w_radps=np.zeros(3)):
+        self.position_m      = position_m
+        self.velocity_mps    = velocity_mps
+        self.Cb2i_dcm        = Cb2i_dcm
+        self.w_radps         = w_radps
+        self.state_vector    = np.array([self.position_m,
+                                         self.velocity_mps,
+                                         self.Cb2i_dcm[0],
+                                         self.Cb2i_dcm[1],
+                                         self.Cb2i_dcm[2],
+                                         self.w_radps])
 
-def state2state_matrix(State):
-    """
-    Inputs: State in class form
-    Outputs: State in its matrix form
-    """
-    return np.array([State.position_m,
-                     State.velocity_mps,
-                     State.attitude_Cb2i_dcm[0],
-                     State.attitude_Cb2i_dcm[1],
-                     State.attitude_Cb2i_dcm[2],
-                     State.w_radps])
+    def update_from_state_vector(self,state_vector):
+        self.state_vector = state_vector
+        self.position_m   = state_vector[0]
+        self.velocity_mps = state_vector[1]
+        self.Cb2i_dcm     =[state_vector[2],
+                            state_vector[3],
+                            state_vector[4]]
+        self.w_radps      = state_vector[5]
+
+   
 
 
-def state_matrix2state(state_matrix, State):
-    """
-    Inputs: State in matrix form
-    Outputs: State in its class form
-    """
-    State.position_m = np.array(state_matrix[0])
-    State.velocity_mps = np.array(state_matrix[1])
-    State.attitude_Cb2i_dcm = np.array([state_matrix[2],
-                                        state_matrix[3],
-                                        state_matrix[4]])
-    State.w_radps = np.array(state_matrix[5])
+class Inputs:
+    '''
+    Stores forces and moments as a vector and variable format
+    '''
+    def __init__(self,forces_n=np.zeros(3),moments_nm=np.zeros(3)):
+        self.forces_n         =  forces_n
+        self.moments_nm       =  moments_nm
+        self.input_vector     =  np.array([forces_n,moments_nm])
 
+    def update_from_input_vector(self,input_vector):
+        self.input_vector     =  input_vector
+        self.forces_n         =  input_vector[0]
+        self.moments_nm       =  input_vector[1]
 
+class MassProperties:
+    '''
+    Contains all vehicles properties
+    '''
+    def __init__(self,mass_kg = -10.0,i_tensor_cg = np.identity(3)):
+        self.mass_kg = mass_kg
+        self.i_tensor_cg = i_tensor_cg
+ 
 """
 ===========================
 State Derivative
 ===========================
 """
 
-
-def get_state_derivative(state_matrix, Inputs, g_mps2, MassProperties):
+def get_state_derivative(state_vector,Inputs,MassProperties):
     """
     Inputs: State in matrix form, Forces and Moments about cg, Gravity , Vehicle Mass and Moment of Inerta Tensor at cg
-    Outputs: Derivative of the current state based on 6dof kinematics as a matrix for integration
+    Outputs: Derivative of the current state based on 6dof kinematics as a vector for integration
     """
 
-    mass_kg = MassProperties.mass_kg  # Current mass of the vehicle
-    i_tensor_cg = MassProperties.i_tensor_cg  # Current moment of inertia tensor at cg
+    # Parse Inputs 
+    position_m   = state_vector[0]
+    velocity_mps = state_vector[1]
+    Cb2i_dcm     =[state_vector[2],
+                   state_vector[3],
+                   state_vector[4]]
+    w_radps      = state_vector[5]
+    forces_n     = Inputs.forces_n
+    moments_nm   = Inputs.moments_nm
+    mass_kg      = MassProperties.mass_kg
+    i_tensor_cg  = MassProperties.i_tensor_cg
 
-    velocity_mps = state_matrix[1]  # Current velocity in meters per second
-    attitude_Cb2i_dcm = [state_matrix[2],
-                         state_matrix[3],
-                         state_matrix[4]]  # Current attitude represented as a DCM from body to inertial frame
-    w_radps = state_matrix[5]  # Current angular rate in radians per second
-
-    forces_n = np.matmul(attitude_Cb2i_dcm,Inputs.forces_n)  # Forces acting in the body frame
+    Inputs.forces_n = np.matmul(Cb2i_dcm,forces_n)  # Forces acting in the body frame
     # (corrected from inertial frame) (eq 3.1-21 and 3.1-12 from strapdown)
 
-    moments_nm = Inputs.moments_nm  # Moments acting about cg
-
     # Kinematics Source: https://en.m.wikipedia.org/wiki/Rigid_body_dynamics
-
     # Resulting acceleration from the forcing acting on the body
     acceleration_mps2 = forces_n / mass_kg
 
     # Resulting angular acceleration due to the moments acting on the body
     wdot_radps2 = np.matmul(np.linalg.inv(i_tensor_cg),
-                            (moments_nm - (np.cross(w_radps,
-                                                    (np.matmul(i_tensor_cg, w_radps))))))
+                            (moments_nm - (np.cross(w_radps,(np.matmul(i_tensor_cg, w_radps))))))
 
     # Converts the current angular rates to a DCM rate
-    attitudedot_Cb2i_dcm = strapdown.rates2dcm(attitude_Cb2i_dcm, w_radps)
+    Cb2idot_dcm = strapdown.rates2dcm(Cb2i_dcm, w_radps)
 
     return np.array(
-        [velocity_mps, acceleration_mps2, attitudedot_Cb2i_dcm[0], attitudedot_Cb2i_dcm[1], attitudedot_Cb2i_dcm[2],
+        [velocity_mps, acceleration_mps2, Cb2idot_dcm[0], Cb2idot_dcm[1], Cb2idot_dcm[2],
          wdot_radps2])
 
 
@@ -88,38 +110,15 @@ Integration
 """
 
 
-def rk4(state_matrix, Inputs, g_mps2, MassProperties, dt):
+def rk4(state_vector, Inputs, MassProperties, dt):
     """
     RK4 Integration Algorithm
     """
-    k1 = get_state_derivative(state_matrix, Inputs, g_mps2, MassProperties)
-    k2 = get_state_derivative(state_matrix + (k1 * dt / 2), Inputs, g_mps2, MassProperties)
-    k3 = get_state_derivative(state_matrix + (k2 * dt / 2), Inputs, g_mps2, MassProperties)
-    k4 = get_state_derivative(state_matrix + (k3 * dt), Inputs, g_mps2, MassProperties)
+    k1 = get_state_derivative(state_vector, Inputs, MassProperties)
+    k2 = get_state_derivative(state_vector + (k1 * dt / 2), Inputs, MassProperties)
+    k3 = get_state_derivative(state_vector + (k2 * dt / 2), Inputs, MassProperties)
+    k4 = get_state_derivative(state_vector + (k3 * dt), Inputs, MassProperties)
 
-    return state_matrix + np.array((k1 + 2 * k2 + 2 * k3 + k4) * dt / 6)
+    return state_vector + np.array((k1 + 2 * k2 + 2 * k3 + k4) * dt / 6)
 
-
-"""
-===========================
-Main Function to Call
-===========================
-"""
-
-
-def get_new_state(State, Inputs, g_mps2, MassProperties, dt):
-    """
-    Inputs: state, forces and moments, dt, gravity, mass and moment of inertia tensor at cg
-    Outputs: the new state based on these parameters
-    """
-
-    state_matrix = state2state_matrix(State)  # For ease of integration and operations
-
-    next_state_matrix = rk4(state_matrix, Inputs, g_mps2, MassProperties, dt)  # Use Rk4 to get the new state
-
-    state_matrix2state(next_state_matrix, State)  # Return to its class form for use in the larger sim
-
-    State.attitude_Cb2i_dcm = strapdown.orthonormalize(State.attitude_Cb2i_dcm)  # Prevents integration an
-    # floating point errors from building
-
-    return None
+    
